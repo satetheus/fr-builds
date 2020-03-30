cb23cf1fe65461a914176ddbf5fddf98dbaefc21
Chris McCullough <37316851+satetheus@users.noreply.github.com>
2020-03-29 22:38:56 -0600

    added y flag to prevent interruptions

--- opencv.sh
+++ opencv.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 # ubuntu 18.04
 
-sudo apt install build-essential cmake git pkg-config libgtk-3-dev \
+sudo apt install -y build-essential cmake git pkg-config libgtk-3-dev \
     libavcodec-dev libavformat-dev libswscale-dev libv4l-dev \
     libxvidcore-dev libx264-dev libjpeg-dev libpng-dev libtiff-dev \
     gfortran openexr libatlas-base-dev python3-dev python3-numpy \
